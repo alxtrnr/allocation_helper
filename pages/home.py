7ef3643ca85ee6ec7001c7d30520fff366280845
Alexander Turner <16516140+alxtrnr@users.noreply.github.com>
2023-03-24 11:53:25 +0000

    Delete home.py

--- pages/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-def app():
-    st.title("Welcome to the Allocations App")
-    st.write("This app allows you to view and manage your patient observations and staff allocations")
-
-
-app()
