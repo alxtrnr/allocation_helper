6a36d069cf4eab0baba0caf8b185c06c2b08add3
Alex Turner <none>
2023-03-24 20:10:23 +0000

    added call for the staff_to_assign table (experimental_data_table). This replaces the display_staff table that was called previously.

--- pages/update_staff_details.py
+++ pages/update_staff_details.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from database_utils.database_operations import update_staff
+from database_utils.database_viewer import staff_to_assign
 
 
 def app():
     st.title("Update Staff")
+    staff_to_assign()
     update_staff()
 
 
