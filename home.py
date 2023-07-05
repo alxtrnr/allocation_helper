2aaec5d481b08639ab1ebfbabe290c108e856aeb
Alex Turner <none>
2023-07-05 21:12:07 +0100

    Reformatted the blurb and layout

--- home.py
+++ home.py
@@ -1,25 +1,40 @@
 import streamlit as st
-st.set_page_config(page_title="Allocate Patient Observations", page_icon="🔭", layout="wide", menu_items=None)
+
+st.set_page_config(page_title="Allocate Patient Observations", page_icon="🔭",
+                   layout="wide", menu_items=None)
 
 
 def app():
-    st.markdown("# The Allocations App")
-    st.markdown("""
+    st.markdown("# The Allocations Helper")
+    st.markdown(
+        "### Use the Allocations Helper to assist with allocating staff to"
+        " patient observations.")
+    st.divider()
+    container = st.container()
+    container.write("")  # used as a spacer
+    col1, col2 = st.columns(2)
+
+    col1.markdown("""
+    #### Functionality includes - 
     * ##### Create, update and delete patient and staff details
     * ##### View patient and staff details
     * ##### Assign staff to complete observations
-    * ##### Complete the allocations
-    * ##### View the allocations
-
-
-    Staff are allocated to patients according to each patients observation level. Staff are assigned by default for 12
-    hours. You may change this for each member of staff according to their availability. Time for breaks is allocated
-    automatically though may also be manually assigned. The same functionality allows any time needed for other
-    tasks such as appointments, meetings, etc to be set. Staff are never assigned for more than two consecutive hours.
-    Requirements for staff of a specific gender to undertake a specific patients observations can be factored in.
-    A requirement to exclude a staff member from a specific patient's observations may also be set. Cherry-picking
-    enables staff to be assigned observations for named patients only.
+    * ##### Generate and view suggested allocations
+    * ##### Download suggested allocations. These can be tweaked as needed.
     """)
+
+    col2.markdown("""
+    #### Generate allocations that factor in -     
+    * ##### Patient observation level (generals, 1:1, 2:1, 3:1, 4:1)
+    * ##### Patient/staff matching as needed
+    * ##### Staff excluded from observations at certain times as needed
+    * ##### No allocation being longer than two contiguous hours
+    * ##### Time off for breaks according to hours worked
+    * ##### Shift patterns - nights/days
+    * ##### Staff working hours - long day/night or custom hours
+    """)
+
+    st.divider()
 
 
 app()
